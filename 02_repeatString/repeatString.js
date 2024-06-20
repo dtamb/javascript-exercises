4b28f72f53e9930bb267b5055d0c409b6835f843
dtamb <89025060+dtamb@users.noreply.github.com>
2024-06-20 22:18:08 +1000

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputString,number) {
+    let outputString = ""
+    if (number <0){
+        outputString ="ERROR";
+    }
+    else{
+        for (let i=0; i<number; i++){
+            outputString += inputString;
+        }
+    }
+    return outputString;
 };
 
 // Do not edit below this line
