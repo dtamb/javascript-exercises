0414ada513330563b4eac879d2f31bf1af55265d
dtamb <89025060+dtamb@users.noreply.github.com>
2024-06-21 17:46:36 +1000

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    let newArray = [];
+    arrayCycle: for (let i=0; i < arguments[0].length; i++){
+        for (let j=1; j<arguments.length; j++){
+            if (arguments[0][i] === arguments[j]){
+                continue arrayCycle;
+            }
+        }
+        newArray.push(arguments[0][i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
