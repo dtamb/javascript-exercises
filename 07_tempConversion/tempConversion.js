57aac8656c94d4e96069f6390272a3b6f7e6839d
dtamb <89025060+dtamb@users.noreply.github.com>
2024-06-21 21:38:44 +1000

    Complete Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF-32)*5/9;
+  return parseFloat(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (9/5*tempC)+32;
+  return parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
