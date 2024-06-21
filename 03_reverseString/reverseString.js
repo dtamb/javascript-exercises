c8300ac694a634da8cadbfa584e0a54eefca04df
dtamb <89025060+dtamb@users.noreply.github.com>
2024-06-21 17:32:26 +1000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reverseString = ""
+    for (let i=inputString.length-1; i >= 0; i--){
+        reverseString += inputString.charAt(i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
