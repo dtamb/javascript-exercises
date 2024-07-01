4a45673f7f324fd725a472fb985b82cfd46cca3a
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-01 18:13:49 +1000

    08 Calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
 	
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,current)=>
+    total + current,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,current)=>
+  total*current);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+  while (n>0){
+    total *= n;
+    n--;
+  }
+  return total;
 };
 
 // Do not edit below this line
