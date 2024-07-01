a1ac2cb023ffaf2506d73a1faea1d7dc0b9fb262
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-01 18:43:14 +1000

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/\W/g, '');
+    input = input.toLowerCase().split('');
+    return input.every((value,index)=> 
+    value === input[input.length - 1 - index]);
 };
-
 // Do not edit below this line
 module.exports = palindromes;
