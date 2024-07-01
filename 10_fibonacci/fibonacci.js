f5ffe44651c4493cde15fab4ac952a9688a937d6
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-01 18:55:31 +1000

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    fib = [0,1,1];
+    n = +n;
+    if (n<0){
+        return "OOPS";
+    }
+    for (let i=2; i < n; i++){
+        fib.push(fib[i]+fib[i-1]);
+    }
+    return fib[n];
 };
 
 // Do not edit below this line
