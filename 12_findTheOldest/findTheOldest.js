247a0894a4d1d30435a8c9dcff3bf8b3892c75af
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-01 19:31:48 +1000

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleAge = people.map(person=>{
+        let age;
+        !person.yearOfDeath 
+            ? age = new Date().getFullYear() - person.yearOfBirth
+            : age = person.yearOfDeath - person.yearOfBirth;
+        return ({...person,["age"]:age})
+    });
+    peopleAge.sort((a,b)=>b.age-a.age);
+    return peopleAge[0];
 };
 
 // Do not edit below this line
